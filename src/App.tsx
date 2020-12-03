6823b12c452a8d466e11515505f15cb74dfdb30f
mokociela <graci.engelman@gmail.com>
2020-12-02 21:07:10 -0300

    Fazer as peças ficarem aleatórias

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,15 @@ const App: React.FC = () => {
     ...getPieces()
   ]
 
+  const boardRandom = board.sort(() => Math.random() - 0.5)
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Joguinho da Memória</h1>
         <p>Jogo da memória para uma pessoa</p>
       </header>
-      <Board pieceList={board} />
+      <Board pieceList={boardRandom} />
     </div>
   );
 }
